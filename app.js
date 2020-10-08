c4d237d92838b65517bc3ffc54bcdc6be0b24a0f
Alexander Vyverman <avyverman@BEC02W22DYHV2H.local>
2020-10-08 17:25:35 +0200

    interval set

--- app.js
+++ app.js
@@ -14,40 +14,45 @@ var Sender = new ZabbixSender({
 });
 const sendHost = 'omnidb1';
 
-tabletojson.convertUrl(
-    dburl,
-    { useFirstRowForHeadings: true })
-    .then(function (js) {
-        var tableList1 = js[0];
-        var tableList2 = tableList1[1];
+setInterval(function () {
+    tabletojson.convertUrl(
+        dburl,
+        { useFirstRowForHeadings: true })
+        .then(function (js) {
+            var tableList1 = js[0];
+            var tableList2 = tableList1[1];
 
-        //mirror role:
-        var mirrorRole = tableList2['MirrorRole'];
-        Sender.addItem(sendHost, 'mirror.role', mirrorRole)
+            //mirror role:
+            var mirrorRole = tableList2['MirrorRole'];
+            Sender.addItem(sendHost, 'mirror.role', mirrorRole)
 
-        //mirror state
-        var mirrorState = tableList2['MirrorState'];
-        Sender.addItem(sendHost, 'mirror.state', mirrorState)
+            //mirror state
+            var mirrorState = tableList2['MirrorState'];
+            Sender.addItem(sendHost, 'mirror.state', mirrorState)
 
-        //Partner state
-        var partnerState = tableList2['PartnerState'];
-        Sender.addItem(sendHost, 'partner.state', partnerState)
+            //Partner state
+            var partnerState = tableList2['PartnerState'];
+            Sender.addItem(sendHost, 'partner.state', partnerState)
 
-        //Arbiter state
-        var arbiterState = tableList2['ArbiterState'];
-        Sender.addItem(sendHost, 'arbiter.state', arbiterState)
+            //Arbiter state
+            var arbiterState = tableList2['ArbiterState'];
+            Sender.addItem(sendHost, 'arbiter.state', arbiterState)
 
-        //stuur naar Zabbix
-        Sender.send(function (err, res) {
-            if (err) {
-                throw err;
-            }
+            //stuur naar Zabbix
+            Sender.send(function (err, res) {
+                if (err) {
+                    throw err;
+                }
 
-            // print the response object
-            console.dir(res);
-        });
-    }
-    );
+                // print the response object
+                console.dir(res);
+            });
+        }
+        );
 
+
+
+
+}, 60000);
 
 
